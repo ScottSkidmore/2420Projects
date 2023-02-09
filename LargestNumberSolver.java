8ce57aa74ec08a226bbd137fb57045fe0262af2c
ScottSkidmore <123768626+ScottSkidmore@users.noreply.github.com>
2023-02-09 02:10:25 -0700

    Add files via upload

--- LargestNumberSolver.java
+++ LargestNumberSolver.java
@@ -2,23 +2,38 @@ package assign04;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
 public class LargestNumberSolver {
+	/**
+	 * This generic method sorts the input array using an
+	 * insertion sort and the input Comparator object.
+	 * @param arr Array to be sorted
+	 * @param cmp The comparator you want to use to sort
+	 * @param <T> The type of array being sorted
+	 */
 	public static <T> void insertionSort(T[] arr, Comparator<? super T> cmp) {
-		for (int i = 1; i < arr.length; i++) {
-			T holder = arr[i];
-			int q = i - 1;
-			while (q >= 0 && cmp.compare(arr[q], holder) < 0) {
-				arr[q + 1] = arr[q];
-				q--;
+		for(int i = 1; i < arr.length; i++){
+			int pos1 = i-1;
+			int pos2 = i;
+			while(cmp.compare(arr[pos1], arr[pos2]) > 0){
+				T temp = arr[pos1];
+				arr[pos1] = arr[pos2];
+				arr[pos2] = temp;
+				if (pos1 > 0){
+					pos2 = pos2 - 1;
+					pos1 = pos1 - 1;
+				}
 			}
-
-			arr[q + 1] = holder;
 		}
 	}
-
+	/**
+	 * This method returns the largest number that can be formed by arranging
+	 * 	the integers of the given array, in any order. If the array is empty,
+	 * 	the largest number that can be formed is 0.
+	 */
 	public static BigInteger findLargestNumber(Integer[] arr) {
 		insertionSort(arr,new ArrayComparator());
 		StringBuilder number = new StringBuilder();
@@ -28,40 +43,99 @@ public class LargestNumberSolver {
 		    BigInteger fin= new BigInteger(numberString);
 		    return fin;
 	}
-
-	public static int findLargestInt(Integer[] arr) throws OutOfRangeException {
+	/**
+	 * This method returns the largest int value that can be formed by arranging the
+	 * integers of the given array, in any order. An OutOfRangeException Download
+	 * OutOfRangeException is thrown if the largest number that can be formed
+	 * is too large for the int data type.  Logic for solving the problem of
+	 * determining the largest number should not appear again in this method —
+	 * call an existing public method or a helper method.
+	 * @param arr
+	 * @return
+	 * @throws OutOfRangeException
+	 */
+	public static int findLargestInt(Integer[] arr) {
 		insertionSort(arr,new ArrayComparator());
 		StringBuilder number = new StringBuilder();
 		for (Integer i : arr)
 			number.append(i);
 			String numberString=number.toString();
+			try {
 		    Integer fin= Integer.parseInt(numberString);
 		    return fin;
+			}
+			catch(Exception OutOfRangeException){
+				throw OutOfRangeException;
+			}
 	}
 
-	public static long findLargestLong(Integer[] arr) throws OutOfRangeException {
-
+	public static long findLargestLong(Integer[] arr) {
+		insertionSort(arr,new ArrayComparator());
+		StringBuilder number = new StringBuilder();
+		for (Integer i : arr)
+			number.append(i);
+			String numberString=number.toString();
+			try {
+		    Long fin= Long.parseLong(numberString);
+		    return fin;
+			}
+			catch(Exception OutOfRangeException){
+				throw OutOfRangeException;
+			}
 	}
 
 	public static BigInteger sum(List<Integer[]> list) {
-		return null;
+		BigInteger sum=new BigInteger("0");
+		for(Integer[] arr:list) {
+			insertionSort(arr,new ArrayComparator());
+			StringBuilder number = new StringBuilder();
+			for (Integer i : arr)
+				number.append(i);
+			BigInteger hold=new BigInteger(number.toString());
+			sum=hold.add(sum);
+		}
+		return sum;
 
 	}
 
-	public static Integer[] findKthLargest(List<Integer[]> list, int k) throws IllegalArgumentException {
-		return null;
+	public static Integer[] findKthLargest(List<Integer[]> lists, int k) throws Exception {
+		if(k>lists.size()-1) {
+			throw new Exception("IllegalArgumentException");
+		}
+		List<Integer[]> list=lists;
+		for(Integer[] arr:list)
+		insertionSort(arr,new ArrayComparator());
+		Integer[] largest=list.get(0);
+		for(int j=0;j<k;j++) {
+			largest=list.get(j);
+		for(int i=j;i<list.size();i++) {
+			if((findLargestNumber(list.get(i)).compareTo(findLargestNumber(largest))>=0)) {
+				largest=list.get(i);
+			}
+			Integer[] hold=list.get(j);
+			int pos=list.indexOf(largest);
+			list.set(j, largest);
+			list.set(pos, list.get(j));
+		}
+
+			}
+		for(Integer[] i:lists)
+		System.out.println(Arrays.toString(i));
+		return list.get(k);
 
 	}
 
 	public static List<Integer[]> readFile(String filename) {
+	
 		return null;
 
 	}
 
-	static private class ArrayComparator<T> implements Comparator<T> {
+
+	public static class ArrayComparator implements Comparator<Integer> {
 
 		@Override
-		public int compare(Object o1, Object o2) {
+		public int compare(Integer o1, Integer o2) {
 			StringBuilder number = new StringBuilder();
 			StringBuilder number2 = new StringBuilder();
 			number.append(o1);
@@ -73,7 +147,7 @@ public class LargestNumberSolver {
 
 	}
 
-	static private class FullArrayComparator implements Comparator<ArrayList<Integer>> {
+	public static class FullArrayComparator implements Comparator<ArrayList<Integer>> {
 
 		@Override
 		public int compare(ArrayList<Integer> o1, ArrayList<Integer> o2) {
@@ -88,3 +162,4 @@ public class LargestNumberSolver {
 
 	}
 }
+
--- LargestNumberSolverTest.java
+++ LargestNumberSolverTest.java
@@ -1,67 +1,91 @@
 package assign04;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-import java.awt.*;
+import java.awt.Point;
+import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
-public class LargestNumberSolverTest extends LargestNumberSolver{
+import assign04.LargestNumberSolver.ArrayComparator;
+import assign04.LargestNumberSolver.FullArrayComparator;
 
-    Integer[] arr1 = {3,1,6,3,10,2,1};
+class LargestNumberSolverTest{
+	 Integer[] arr1 = {3,1,6,3,10,2,1};
+	 Integer[] arr3 = {1,43,23,12,1,43,23,12,1,43,23,12};
+	 Integer[] arr4 = {1,2};
+	 Integer[] arr5 = {789,36};
+	 Integer[] arr6 = {28984,29};
+	 
+	 ArrayList<Integer[]> numbers = new ArrayList<>();
+	 
 
-    Point p1 = new Point(5,6);
-    Point p2 = new Point(3,5);
-    Point p3 = new Point(9,2);
-    Point p4 = new Point(1,0);
-    Point p5 = new Point(0,1);
-    Point[] arr2 = {p1,p2,p3,p4,p5};
-    Comparator<Integer> intSort = Comparator.naturalOrder();
-    Comparator<Point> pointSort = (Point o1, Point o2) -> (int)(o1.getX() - o2.getX());
-    @Test
-    void insertionSortTest(){
-        insertionSort(arr1,intSort);
-        System.out.print(Arrays.toString(arr1));
-        assertEquals(1, arr1[0]);
-    }
+	    Point p1 = new Point(5,6);
+	    Point p2 = new Point(3,5);
+	    Point p3 = new Point(9,2);
+	    Point p4 = new Point(1,0);
+	    Point p5 = new Point(0,1);
+	    Point[] arr2 = {p1,p2,p3,p4,p5};
+	    Comparator<Integer> intSort = Comparator.naturalOrder();
+	    Comparator<Point> pointSort = (Point o1, Point o2) -> (int)(o1.getX() - o2.getX());
+	    @Test
+	    void insertionSortTest(){
+	        LargestNumberSolver.insertionSort(arr1,intSort);
+	        //System.out.print(Arrays.toString(arr1));
+	        assertEquals(1, arr1[0]);
+	    }
 
-    @Test
-    void insertionSortTest2(){
-        insertionSort(arr2,pointSort);
-        System.out.print(Arrays.toString(arr2));
-        assertEquals(new Point(0,1), arr2[0]);
-    }
+	    @Test
+	    void insertionSortTest2(){
+	    	LargestNumberSolver.insertionSort(arr2,pointSort);
+	        //System.out.print(Arrays.toString(arr2));
+	        assertEquals(new Point(0,1), arr2[0]);
+	    }
 
 
-    @Test
-    void findLargestNumberTest(){
-        System.out.print(findLargestNumber(arr1));
-    }
+	    @Test
+	    void findLargestNumberTest(){
+	    	int i=63321110;
+	        LargestNumberSolver.insertionSort(arr1,new ArrayComparator());
+	        //System.out.println(LargestNumberSolver.findLargestNumber(arr1));
+	        assertEquals(i,LargestNumberSolver.findLargestInt(arr1));
+	    }
 
-    @Test
-    void findLargestIntTest(){
+	    @Test
+	    void findLargestIntTest(){
 
-    }
+	    }
 
-    @Test
-    void findLargestLongTest(){
+	    @Test
+	    void findLargestLongTest(){
 
-    }
+	    }
 
-    @Test
-    void sumTest(){
+	    @Test
+	    void sumTest(){
+	    	numbers.add(arr3);
+	    	numbers.add(arr1);
+	    	//System.out.println(LargestNumberSolver.sum(numbers));
+	    	assertEquals(67644231,LargestNumberSolver.sum(numbers));
+	    	
+	    }
 
-    }
+	    @Test
+	    void findKthLargestTest() throws Exception{
+	    	numbers.add(arr3);
+	    	numbers.add(arr1);
+	    	numbers.add(arr4);
+	    	numbers.add(arr5);
+	    	numbers.add(arr6);
+	    	//System.out.println(Arrays.toString(LargestNumberSolver.findKthLargest(numbers,2)));
+	    	assertEquals(arr4,LargestNumberSolver.findKthLargest(numbers,4));
+	    }
+	    @Test
+	    void readFileTest(){
 
-    @Test
-    void findKthLargestTest(){
-
-    }
-    @Test
-    void readFileTest(){
-
-    }
+	    }
 
 }
