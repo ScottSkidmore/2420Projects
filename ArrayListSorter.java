da0ff679fae7ff20adbf9de88817640414d2168c
scottskidmore <123768626+scottskidmore@users.noreply.github.com>
2023-02-23 00:18:43 -0700

    Add files via upload

--- ArrayListSorter.java
+++ ArrayListSorter.java
@@ -1,97 +1,252 @@
 package assign05;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
+/**
+ * This class contains methods that run merge sort and quick sort and has
+ * methods to generate arrays to test the two sorting methods on.
+ * 
+ * @author Scott Skidmore and Nate Zuro
+ * @Version February 22nd, 2023
+ */
 public class ArrayListSorter<T> {
-		public static <T extends Comparable<? super T>> void mergesort(ArrayList<T> list) {
-		    mergesortRun(list, 0, list.size() - 1);
-		}
+	private static int insertionPoint = 10;
 
-		private static <T extends Comparable<? super T>> void mergesortRun(ArrayList<T> list, int low, int high) {
-		    if (high-low<=10) {
-		    	
-		    }
-		    else {
-		        int middle = (low + high) / 2;
-		        mergesortRun(list, low, middle);
-		        mergesortRun(list, middle + 1, high);
-		        ArrayList<T> temp = new ArrayList<>(high - low + 1);
-		        merge(list, low, middle, high,temp);
-		    }
-		}
+	/**
+	 * method provides a low and high value for merge sort run to allow for a
+	 * simpler call of merge sort.
+	 * 
+	 * @param <T>  generic type of the array
+	 * @param list the array being sorted
+	 */
+	public static <T extends Comparable<? super T>> void mergesort(ArrayList<T> list) {
+		mergesortRun(list, 0, list.size() - 1);
+	}
 
-		private static <T extends Comparable<? super T>> void merge(ArrayList<T> list, int low, int middle, int high,ArrayList<T> temp) {
-		    int i = low;
-		    int j = middle + 1;
-		    while (i <= middle && j <= high) {
-		        if (list.get(i).compareTo(list.get(j)) <= 0) {
-		            temp.add(list.get(i));
-		            i++;
-		        } else {
-		            temp.add(list.get(j));
-		            j++;
-		        }
-		    }
-		    while (i <= middle) {
-		        temp.add(list.get(i));
-		        i++;
-		    }
-		    while (j <= high) {
-		        temp.add(list.get(j));
-		        j++;
-		    }
-		    for (int q = 0; q < temp.size(); q++) {
-		        list.set(low + q, temp.get(q));
-		    }
+	/**
+	 * This method calls merge sort recursively and then merges the divided array
+	 * back into a sorted array using the merge method. Once the sub arrays reach a
+	 * certain size merge sort will begin to use insertion sort to sort the smaller
+	 * arrays
+	 * 
+	 * @param <T>  generic type of the array
+	 * @param list array being sorted
+	 * @param low  the lower index of the current array
+	 * @param high the high index of the current array
+	 */
+	private static <T extends Comparable<? super T>> void mergesortRun(ArrayList<T> list, int low, int high) {
+		if (high - low <= insertionPoint) {
+			for (int i = low + 1; i <= high; i++) {
+				int pos1 = i - 1;
+				int pos2 = i;
+				while (list.get(pos1).compareTo(list.get(pos2)) > 0) {
+					T temp = list.get(pos1);
+					list.set(pos1, list.get(pos2));
+					list.set(pos2, temp);
+					if (pos1 > low) {
+						pos2 = pos2 - 1;
+						pos1 = pos1 - 1;
+					}
+				}
+			}
+		} else {
+			int middle = (low + high) / 2;
+			mergesortRun(list, low, middle);
+			mergesortRun(list, middle + 1, high);
+			ArrayList<T> temp = new ArrayList<>(high - low + 1);
+			merge(list, low, middle, high, temp);
 		}
+	}
 
+	/**
+	 * This method merges the divided arrays created by merge sort into a fully
+	 * sorted array.
+	 * 
+	 * @param <T>    generic type of the array
+	 * @param list   the array being sorted
+	 * @param low    the low index of sub array or full array
+	 * @param middle the middle index of array or sub array
+	 * @param high   the high index of array or sub array
+	 * @param temp   a temporary array to hold values
+	 */
+	private static <T extends Comparable<? super T>> void merge(ArrayList<T> list, int low, int middle, int high,
+			ArrayList<T> temp) {
+		int i = low;
+		int j = middle + 1;
+		while (i <= middle && j <= high) {
+			if (list.get(i).compareTo(list.get(j)) <= 0) {
+				temp.add(list.get(i));
+				i++;
+			} else {
+				temp.add(list.get(j));
+				j++;
+			}
+		}
+		while (i <= middle) {
+			temp.add(list.get(i));
+			i++;
+		}
+		while (j <= high) {
+			temp.add(list.get(j));
+			j++;
+		}
+		for (int q = 0; q < temp.size(); q++) {
+			list.set(low + q, temp.get(q));
+		}
+	}
+
+	/**
+	 * method provides a low and high value for quick sort run to allow for a
+	 * simpler call of quick sort.
+	 * 
+	 * @param <T>  generic type of the array
+	 * @param list the array being sorted
+	 */
 	public static <T extends Comparable<? super T>> void quicksort(ArrayList<T> list) {
 		quicksortRun(list, 0, list.size() - 1);
 	}
 
+	/**
+	 * This method calls quick sort recursively and then uses partition to divide
+	 * the array and sort it as it goes.
+	 * 
+	 * @param <T>  generic type of the array
+	 * @param list array being sorted
+	 * @param low  the lower index of the current array
+	 * @param high the high index of the current array
+	 */
 	private static <T extends Comparable<? super T>> void quicksortRun(ArrayList<T> list, int low, int high) {
-	    if (low < high) {
-	        int divide = partition(list, low, high);
-	        quicksortRun(list, low, divide - 1);
-	        quicksortRun(list, divide + 1, high);
-	    }
+		if (low < high) {
+			int divide = partition(list, low, high);
+			quicksortRun(list, low, divide - 1);
+			quicksortRun(list, divide + 1, high);
+		}
 	}
 
+	/**
+	 * helper method to create a random pivot point
+	 * 
+	 * @param <T>  generic type of the array
+	 * @param list the array being sorted
+	 * @param low  the low index of sub array or full array
+	 * @param high the high index of array or sub array
+	 * @return returns the index for the pivot
+	 */
+	private static <T extends Comparable<? super T>> int randomPivot(ArrayList<T> list, int low, int high) {
+		return new Random().nextInt(high - low) + low;
+	}
+
+	/**
+	 * helper method to create a high pivot point
+	 * 
+	 * @param <T>  generic type of the array
+	 * @param list the array being sorted
+	 * @param low  the low index of sub array or full array
+	 * @param high the high index of array or sub array
+	 * @return returns the index for the pivot
+	 */
+	private static <T extends Comparable<? super T>> int highPivot(ArrayList<T> list, int low, int high) {
+		return high;
+	}
+
+	/**
+	 * helper method to create a pivot point in the middle
+	 * 
+	 * @param <T>  generic type of the array
+	 * @param list the array being sorted
+	 * @param low  the low index of sub array or full array
+	 * @param high the high index of array or sub array
+	 * @return returns the index for the pivot
+	 */
+	private static <T extends Comparable<? super T>> int middlePivot(ArrayList<T> list, int low, int high) {
+		return low + (high - low) / 2;
+	}
+
+	/**
+	 * This method picks a pivot point and sorts the array or sub array based on the
+	 * pivot point given
+	 * 
+	 * @param <T>  generic type of the array
+	 * @param list the array being sorted
+	 * @param low  the low index of sub array or full array
+	 * @param high the high index of array or sub array
+	 * @return returns the index that the array was sorted around
+	 */
 	private static <T extends Comparable<? super T>> int partition(ArrayList<T> list, int low, int high) {
-	    //int pivotIndex= new Random().nextInt(high-low)+low;
-		T pickPivot = list.get(high);
-		//flip(list,pivotIndex,high);
-	    int i = low - 1;
-	    for (int j = low; j < high; j++) {
-	        if (list.get(j).compareTo(pickPivot) <= 0) {
-	            i++;
-	            flip(list, i, j);
-	        }
-	    }
-	    flip(list, i + 1, high);
-	    return i + 1;
+		int pivotIndex = randomPivot(list, low, high);
+		//int pivotIndex = highPivot(list, low, high);
+		//int pivotIndex = middlePivot(list, low, high);
+		T pickPivot = list.get(pivotIndex);
+		flip(list, pivotIndex, high);
+		int i = low - 1;
+		for (int j = low; j < high; j++) {
+			if (list.get(j).compareTo(pickPivot) <= 0) {
+				i++;
+				flip(list, i, j);
+			}
+		}
+		flip(list, i + 1, high);
+		return i + 1;
 	}
 
+	/**
+	 * flips two item indexes in the array
+	 * 
+	 * @param <T>  Generic type of the array
+	 * @param list the array being sorted
+	 * @param i    the index of first item being flipped
+	 * @param j    the index of second item being flipped
+	 */
 	private static <T extends Comparable<? super T>> void flip(ArrayList<T> list, int i, int j) {
-	    T holder = list.get(i);
-	    list.set(i, list.get(j));
-	    list.set(j, holder);
+		T holder = list.get(i);
+		list.set(i, list.get(j));
+		list.set(j, holder);
 	}
-	
-	public static ArrayList<Integer> generateAscending(int size){
-		return null;
-		
+
+	/**
+	 * generates an array list from one to size in ascending order
+	 * 
+	 * @param size the size of the array
+	 * @return the array in ascending order
+	 */
+	public static ArrayList<Integer> generateAscending(int size) {
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		for (int i = 1; i <= size; i++) {
+			arr.add(i);
+		}
+		return arr;
 	}
-	public static ArrayList<Integer> generatePermuted(int size){
-		return null;
-		
+
+	/**
+	 * generates an array list from one to size in random order
+	 * 
+	 * @param size the size of the array
+	 * @return the array in random order
+	 */
+	public static ArrayList<Integer> generatePermuted(int size) {
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		for (int i = 1; i <= size; i++) {
+			arr.add(i);
+		}
+		Collections.shuffle(arr);
+		return arr;
 	}
-	public static ArrayList<Integer> generateDescending(int size){
-		return null;
-		
+
+	/**
+	 * generates an array list from one to size in descending order
+	 * 
+	 * @param size the size of the array
+	 * @return the array in descending order
+	 */
+	public static ArrayList<Integer> generateDescending(int size) {
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		for (int i = size; i >= 1; i--) {
+			arr.add(i);
+		}
+		return arr;
 	}
+
 }
-		
-	
--- ArrayListSorterTest.java
+++ ArrayListSorterTest.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 class ArrayListSorterTest {
-	ArrayList<Integer> list1=new ArrayList<Integer>();
+	ArrayList<Integer> list1 = new ArrayList<Integer>();
 
 	@Test
 	void test() {
@@ -18,9 +18,8 @@ class ArrayListSorterTest {
 		list1.add(5);
 		ArrayListSorter.mergesort(list1);
 		System.out.println(list1.toString());
-		
-		fail("Not yet implemented");
 	}
+
 	@Test
 	void test1() {
 		list1.add(1);
@@ -29,8 +28,246 @@ class ArrayListSorterTest {
 		list1.add(5);
 		ArrayListSorter.quicksort(list1);
 		System.out.println(list1.toString());
-		
-		fail("Not yet implemented");
 	}
+
+	@Test
+	void generateAscendingTest() {
+		ArrayList<Integer> arr = ArrayListSorter.generateAscending(9);
+		assertEquals(9, arr.get(8));
+		assertEquals(4, arr.get(3));
+		assertEquals(1, arr.get(0));
+	}
+
+	@Test
+	void generateAscendingTestLarge() {
+		ArrayList<Integer> arr = ArrayListSorter.generateAscending(30);
+		assertEquals(30, arr.get(29));
+		assertEquals(15, arr.get(14));
+		assertEquals(1, arr.get(0));
+	}
+
+	@Test
+	void generatePermutedTest() {
+		ArrayList<Integer> arr = ArrayListSorter.generatePermuted(8);
+		if (!arr.equals(ArrayListSorter.generateAscending(8)) && !arr.equals(ArrayListSorter.generateDescending(8))) {
+			assertEquals(0, 0);
+		} else
+			assertEquals(0, 1);
+	}
+
+	@Test
+	void generatePermutedTestLarge() {
+		ArrayList<Integer> arr = ArrayListSorter.generatePermuted(25);
+		if (!arr.equals(ArrayListSorter.generateAscending(25)) && !arr.equals(ArrayListSorter.generateDescending(25))) {
+			assertEquals(0, 0);
+		} else
+			assertEquals(0, 1);
+	}
+
+	@Test
+	void generateDescendingTest() {
+		ArrayList<Integer> arr = ArrayListSorter.generateDescending(10);
+		assertEquals(10, arr.get(0));
+		assertEquals(4, arr.get(6));
+		assertEquals(1, arr.get(9));
+	}
+
+	@Test
+	void generateDescendingTestLarge() {
+		ArrayList<Integer> arr = ArrayListSorter.generateDescending(27);
+		assertEquals(27, arr.get(0));
+		assertEquals(20, arr.get(7));
+		assertEquals(1, arr.get(26));
+	}
+	 @Test
+	    void testMergeVeryEmpty() {
+	        ArrayList<Integer> arr = ArrayListSorter.generatePermuted(0);
+	        ArrayList<Integer> arr2 = new ArrayList<>();
+	        ArrayListSorter.mergesort(arr);
+	        if(arr.equals(arr2))return;
+	        fail("Lists are not equal");
+	    }
+
+	    @Test
+	    void testMergeVerySmall() {
+	        ArrayList<Integer> arr = ArrayListSorter.generatePermuted(1);
+	        ArrayListSorter.mergesort(arr);
+	        assertEquals(1, arr.get(0));
+	    }
+
+	    @Test
+	    void testMergeSmallRandom() {
+	        ArrayList<Integer> arr = ArrayListSorter.generatePermuted(10);
+	        ArrayList<Integer> sorted = ArrayListSorter.generateAscending(10);
+	        ArrayListSorter.mergesort(arr);
+	        if(arr.equals(sorted))return;
+	        fail("Lists are not equal");
+
+	    }
+	    @Test
+	    void testMergeSmallSorted() {
+	        ArrayList<Integer> arr = ArrayListSorter.generateAscending(9);
+	        ArrayList<Integer> arr2 = ArrayListSorter.generateAscending(9);
+	        ArrayListSorter.mergesort(arr);
+	        if(arr.equals(arr2))return;
+	        fail("Lists are not equal");
+	    }
+	    @Test
+	    void testMergeSmallReversed() {
+	        ArrayList<Integer> arr = ArrayListSorter.generateDescending(13);
+	        ArrayList<Integer> arr2 = ArrayListSorter.generateAscending(13);
+	        ArrayListSorter.mergesort(arr);
+	        if(arr.equals(arr2))return;
+	        fail("Lists are not equal");
+	    }
+
+	    @Test
+	    void testMergeMediumRandom() {
+	        ArrayList<Integer> arr = ArrayListSorter.generatePermuted(50);
+	        ArrayList<Integer> arr2 = ArrayListSorter.generateAscending(50);
+	        ArrayListSorter.mergesort(arr);
+	        if(arr.equals(arr2))return;
+	        fail("Lists are not equal");
+	    }
+
+	    @Test
+	    void testMergeMediumSorted() {
+	        ArrayList<Integer> arr = ArrayListSorter.generateAscending(51);
+	        ArrayList<Integer> arr2 = ArrayListSorter.generateAscending(51);
+	        ArrayListSorter.mergesort(arr);
+	        if(arr.equals(arr2))return;
+	        fail("Lists are not equal");
+	    }
+
+	    @Test
+	    void testMergeMediumReversed() {
+	        ArrayList<Integer> arr = ArrayListSorter.generateDescending(72);
+	        ArrayList<Integer> arr2 = ArrayListSorter.generateAscending(72);
+	        ArrayListSorter.mergesort(arr);
+	        if(arr.equals(arr2))return;
+	        fail("Lists are not equal");
+	    }
+
+	    @Test
+	    void testMergeLargeRandom() {
+	        ArrayList<Integer> arr = ArrayListSorter.generatePermuted(123);
+	        ArrayList<Integer> arr2 = ArrayListSorter.generateAscending(123);
+	        ArrayListSorter.mergesort(arr);
+	        if(arr.equals(arr2))return;
+	        fail("Lists are not equal");
+	    }
+
+	    @Test
+	    void testMergeLargeSorted() {
+	        ArrayList<Integer> arr = ArrayListSorter.generateAscending(110);
+	        ArrayList<Integer> arr2 = ArrayListSorter.generateAscending(110);
+	        ArrayListSorter.mergesort(arr);
+	        if(arr.equals(arr2))return;
+	        fail("Lists are not equal");
+	    }
+
+	    @Test
+	    void testMergeLargeReversed() {
+	        ArrayList<Integer> arr = ArrayListSorter.generateDescending(187);
+	        ArrayList<Integer> arr2 = ArrayListSorter.generateAscending(187);
+	        ArrayListSorter.mergesort(arr);
+	        if(arr.equals(arr2))return;
+	        fail("Lists are not equal");
+	    }
+	    @Test
+	    void testQuickVeryEmpty() {
+	        ArrayList<Integer> arr = ArrayListSorter.generatePermuted(0);
+	        ArrayList<Integer> arr2 = new ArrayList<>();
+	        ArrayListSorter.quicksort(arr);
+	        if(arr.equals(arr2))return;
+	        fail("Lists are not equal");
+	    }
+
+	    @Test
+	    void testQuickVerySmall() {
+	        ArrayList<Integer> arr = ArrayListSorter.generatePermuted(1);
+	        ArrayListSorter.quicksort(arr);
+	        assertEquals(1, arr.get(0));
+	    }
+
+	    @Test
+	    void testQuickSmallRandom() {
+	        ArrayList<Integer> arr = ArrayListSorter.generatePermuted(10);
+	        ArrayList<Integer> sorted = ArrayListSorter.generateAscending(10);
+	        ArrayListSorter.quicksort(arr);
+	        if(arr.equals(sorted))return;
+	        fail("Lists are not equal");
+
+	    }
+	    @Test
+	    void testQuickSmallSorted() {
+	        ArrayList<Integer> arr = ArrayListSorter.generateAscending(9);
+	        ArrayList<Integer> arr2 = ArrayListSorter.generateAscending(9);
+	        ArrayListSorter.quicksort(arr);
+	        if(arr.equals(arr2))return;
+	        fail("Lists are not equal");
+	    }
+	    @Test
+	    void testQuickSmallReversed() {
+	        ArrayList<Integer> arr = ArrayListSorter.generateDescending(13);
+	        ArrayList<Integer> arr2 = ArrayListSorter.generateAscending(13);
+	        ArrayListSorter.quicksort(arr);
+	        if(arr.equals(arr2))return;
+	        fail("Lists are not equal");
+	    }
+
+	    @Test
+	    void testQuickMediumRandom() {
+	        ArrayList<Integer> arr = ArrayListSorter.generatePermuted(50);
+	        ArrayList<Integer> arr2 = ArrayListSorter.generateAscending(50);
+	        ArrayListSorter.quicksort(arr);
+	        if(arr.equals(arr2))return;
+	        fail("Lists are not equal");
+	    }
+
+	    @Test
+	    void testQuickMediumSorted() {
+	        ArrayList<Integer> arr = ArrayListSorter.generateAscending(51);
+	        ArrayList<Integer> arr2 = ArrayListSorter.generateAscending(51);
+	        ArrayListSorter.quicksort(arr);
+	        if(arr.equals(arr2))return;
+	        fail("Lists are not equal");
+	    }
+
+	    @Test
+	    void testQuickMediumReversed() {
+	        ArrayList<Integer> arr = ArrayListSorter.generateDescending(72);
+	        ArrayList<Integer> arr2 = ArrayListSorter.generateAscending(72);
+	        ArrayListSorter.quicksort(arr);
+	        if(arr.equals(arr2))return;
+	        fail("Lists are not equal");
+	    }
+
+	    @Test
+	    void testQuickLargeRandom() {
+	        ArrayList<Integer> arr = ArrayListSorter.generatePermuted(123);
+	        ArrayList<Integer> arr2 = ArrayListSorter.generateAscending(123);
+	        ArrayListSorter.quicksort(arr);
+	        if(arr.equals(arr2))return;
+	        fail("Lists are not equal");
+	    }
+
+	    @Test
+	    void testQuickLargeSorted() {
+	        ArrayList<Integer> arr = ArrayListSorter.generateAscending(110);
+	        ArrayList<Integer> arr2 = ArrayListSorter.generateAscending(110);
+	        ArrayListSorter.quicksort(arr);
+	        if(arr.equals(arr2))return;
+	        fail("Lists are not equal");
+	    }
+
+	    @Test
+	    void testQuickLargeReversed() {
+	        ArrayList<Integer> arr = ArrayListSorter.generateDescending(187);
+	        ArrayList<Integer> arr2 = ArrayListSorter.generateAscending(187);
+	        ArrayListSorter.quicksort(arr);
+	        if(arr.equals(arr2))return;
+	        fail("Lists are not equal");
+	    }
 
 }
