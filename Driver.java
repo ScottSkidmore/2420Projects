a83a001d04804de47dea0256ed7d2564ed2028a5
natezuro <124845846+natezuro@users.noreply.github.com>
2023-04-25 15:14:02 -0600

    Add files via upload

--- Driver.java
+++ Driver.java
@@ -5,43 +5,49 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Driver {
-	public static void main(String[] args) throws FileNotFoundException {
-		File read = new File(args[0]);
-		DisjointForest<String>dj=new DisjointForest<String>();
-		try {
-			Scanner reader = new Scanner(read);
-			while (reader.hasNextLine()) {
-				String line =reader.next();
-				//System.out.println(line);
-				if (line=="") {
-					while (reader.hasNextLine()) {
-						line=reader.next();
-						if(line=="") {
-					
-						
-							while(reader.hasNext()) {
-								line=reader.next();
-								if(dj.getRepresentative(""+line.charAt(0))==dj.getRepresentative(""+line.charAt(3))) {
-									System.out.println("connected");
-								}else {
-									System.out.println("not connected");
-								}
-							}
-						}else {
-							dj.union(""+line.charAt(0), ""+line.charAt(3));
-						}
-					}
-					
-				}else {
-					dj.makeSet(line);
-				}
-				
-			}
-			reader.close();
-		} catch (Exception e) {
-			e= new FileNotFoundException();
-			System.out.println(e);
-		}
+    public static void main(String[] args) throws FileNotFoundException {
 
-	}
+
+        File read = new File(args[0]);
+        DisjointForest<String>dj=new DisjointForest<String>();
+        try {
+            Scanner reader = new Scanner(read);
+
+            while (reader.hasNextLine()) {
+                String line =reader.nextLine();
+
+                if (line.isEmpty()) {
+                    while (reader.hasNextLine()) {
+
+                        line=reader.nextLine();
+
+                        if(line.isEmpty()) {
+
+
+                            while(reader.hasNext()) {
+                                line=reader.nextLine();
+
+                                if(dj.getRepresentative(""+line.charAt(0))==dj.getRepresentative(""+line.charAt(2))) {
+                                    System.out.println("connected");
+                                }else {
+                                    System.out.println("not connected");
+                                }
+                            }
+                        }else {
+                            dj.union(""+line.charAt(0), ""+line.charAt(2));
+                        }
+                    }
+
+                }else {
+                    dj.makeSet(line);
+                }
+
+            }
+            reader.close();
+        } catch (Exception e) {
+            e= new FileNotFoundException();
+            System.out.println(e);
+        }
+
+   }
 }
