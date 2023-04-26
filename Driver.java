826db556f501165a11599027b325e71757a39731
scottskidmore <123768626+scottskidmore@users.noreply.github.com>
2023-04-25 23:42:15 -0600

    Add files via upload

--- Driver.java
+++ Driver.java
@@ -3,51 +3,48 @@ package comprehensive;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-
+/**
+ * Class to allow the disjoint set to run using a file fed from 
+ * the command line.
+ * @author Scott and Nate
+ *
+ */
 public class Driver {
     public static void main(String[] args) throws FileNotFoundException {
-
-
-        File read = new File(args[0]);
-        DisjointForest<String>dj=new DisjointForest<String>();
+        DisjointForest<String> dj = new DisjointForest<String>();
+        File file = new File(args[0]);
         try {
-            Scanner reader = new Scanner(read);
+            try (Scanner scanner = new Scanner(file)) {
+				while (scanner.hasNextLine()) {
+				    String nextLine = scanner.nextLine();
 
-            while (reader.hasNextLine()) {
-                String line =reader.nextLine();
+				    if (nextLine.isEmpty()) break;
 
-                if (line.isEmpty()) {
-                    while (reader.hasNextLine()) {
+				    dj.makeSet(nextLine);
+				}
+				while (scanner.hasNextLine()) {
+				    String nextLine = scanner.nextLine();
 
-                        line=reader.nextLine();
+				    if (nextLine.isEmpty()) break;
 
-                        if(line.isEmpty()) {
+				    String[] line = nextLine.split("\\s+");
+				    dj.union(line[1], line[0]);
+				}
+				while (scanner.hasNextLine()) {
+				    String nextLine = scanner.nextLine();
 
+				    String[] elements = nextLine.split("\\s+");
 
-                            while(reader.hasNext()) {
-                                line=reader.nextLine();
-
-                                if(dj.getRepresentative(""+line.charAt(0))==dj.getRepresentative(""+line.charAt(2))) {
-                                    System.out.println("connected");
-                                }else {
-                                    System.out.println("not connected");
-                                }
-                            }
-                        }else {
-                            dj.union(""+line.charAt(0), ""+line.charAt(2));
-                        }
-                    }
-
-                }else {
-                    dj.makeSet(line);
-                }
-
-            }
-            reader.close();
-        } catch (Exception e) {
-            e= new FileNotFoundException();
-            System.out.println(e);
+				    if (nextLine.isEmpty()) break;
+				    if (!dj.getRepresentative(elements[0]).equals(dj.getRepresentative(elements[1]))) {
+				        System.out.println("not connected");
+				    } else {
+				        System.out.println("connected");
+				    }
+				}
+			}
+        } catch (FileNotFoundException e) {
+            System.out.println("file not found");
         }
-
-   }
+    }
 }
--- Node.java
+++ Node.java
@@ -1,5 +1,11 @@
 package comprehensive;
-
+/**
+ * Class creates a node for the disjoint set with the 
+ * needed functionality
+ * @author Scott and Nate
+ *
+ * @param <E>
+ */
 public class Node<E> {
 
     Node<E> representative = null;
@@ -7,25 +13,47 @@ public class Node<E> {
     E data;
     
     int rank;
-
+    /**
+     * constructs the node assigning the rank and 
+     * the data it is holding
+     * @param element the data it will hold
+     */
     public Node(E element){
         this.data= element;
         rank=1;
     }
-
+/**
+ * setter for the representative field
+ * @param N the node we want to set the field as
+ */
     public void setRepresentative(Node<E> N){
         this.representative = N;
     }
-
+/**
+ * getter for the representative field
+ * @return the representative node
+ */
     public Node<E> get(){
         return representative;
     }
+    /**
+     * getter for the data
+     * @return the data of the node
+     */
     public E getElement(){
         return data;
     }
+    /**
+     * getter for the rank of the node
+     * @return the rank value
+     */
     public int getRank(){
         return rank;
     }
+    /**
+     * setter for the rank value of a node
+     * @param n value we want the rank
+     */
     public void setRank(int n){
         this.rank = n;
     }
